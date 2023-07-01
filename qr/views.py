f934441d80d08de6bb68d46359b47e3f6b73d880
sobit-nep <nepalsobit1@gmail.com>
2023-07-01 20:34:01 +0545

    files commited

---
+++ qr/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+import qrcode
+from PIL import Image
+from io import BytesIO
+import base64
+
+def index(request):
+    context = {}
+    if request.method == "POST":
+        qr_text = request.POST.get("qr_text", "")
+        qr_image = qrcode.make(qr_text, box_size=15)
+        qr_image_pil = qr_image.get_image()
+        stream = BytesIO()
+        qr_image_pil.save(stream, format='PNG')
+        qr_image_data = stream.getvalue()
+        qr_image_base64 = base64.b64encode(qr_image_data).decode('utf-8')
+        context['qr_image_base64'] = qr_image_base64
+        context['variable'] = qr_text
+    return render(request, 'index.html', context=context)
